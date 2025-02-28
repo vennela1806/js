d211f06011b13d922c0cece139a2a22f086320f3
vennela1806 <koutharapuvennela6@gmail.com>
2025-02-28 13:21:03 +0530

    first commit

---
+++ basic.jsx
@@ -0,0 +1,19 @@
+const array = ["vennela", 87, true, { name: "vennela " }]
+
+console.log(typeof array)
+
+//output : object we will get type as objectonly even it is array in JS 
+//BUt to know whether it is array or not we use
+
+console.log(Array.isArray(array))
+//if it is array we will get true as output
+
+
+const obj = {
+    name: "vennela",
+    age: 27,
+    married: true
+} 
+
+console.log(obj)
+
