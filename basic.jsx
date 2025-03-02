37295f3237d69ccbf65acfb237e4b3097d480eed
vennela1806 <koutharapuvennela6@gmail.com>
2025-03-02 23:05:48 +0530

    push origin testing

--- basic.jsx
+++ basic.jsx
@@ -1,25 +1,14 @@
-const array = ["vennela", 87, true, { name: "vennela " }]
+function higherOrder(fn) {
+    fn();
+}
 
-console.log(typeof array)
+higherOrder(function () {
 
-//output : object we will get type as objectonly even it is array in JS 
-//BUt to know whether it is array or not we use
-
-console.log(Array.isArray(array))
-//if it is array we will get true as output
-
-
-const obj = {
-    name: "vennela",
-    age: 27,
-    married: true
-} 
+    console.log("Hello world")
 
 
 
-console.log(obj)
-
-console.log("second commit")
 
 
-console.log("third commit")
+    
+});
